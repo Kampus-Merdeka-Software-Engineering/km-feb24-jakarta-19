d1a516b28a8ee6533fe81cd6bc024c588537fc7c
Fatimaawr <fatimahwanudyararas@gmail.com>
2024-05-31 22:25:00 +0700

    update filter

--- js/script.js
+++ js/script.js
@@ -2,22 +2,10 @@
 const menuBtn = document.getElementById('menu-label');
 const sidebar = document.getElementsByClassName('sidebar')[0];
 
-menuBtn.addEventListener('click', function(){
+menuBtn.addEventListener('click', function () {
     sidebar.classList.toggle('hide');
 
 });
-// const mainSect = document.getElementsByClassName('main-section')[0];
-// const aside = document.getElementsByTagName('aside')[0];
-
-// menuBtn.addEventListener('click', function (e) {
-//     aside.classList.toggle('hide');
-// })
-
-// document.addEventListener('click', (e) => {
-//     if (!aside.contains(e.target) && !menuBtn.contains(e.target)) {
-//         aside.classList.add('hide');
-//     }
-// });
 // TOGGLE SIDEBAR
 
 import datasets from "./../asset/dataset coffee shop selas.json" assert {
@@ -62,28 +50,95 @@ for (let index = 0; index < datasets.length; index++) {
 
         })
     }
-    console.log(dataset)
+    // console.log(dataset)
 
-    document.querySelector('#colom').innerHTML += `
-    <tr>
-    <td>${index +1}</td>
-    <td>${dataset.transaction_id}</td>
-    <td>${dataset.transaction_date}</td>
-    <td>${dataset.transaction_time}</td>
-    <td>${dataset.transaction_qty}</td>
-    <td>${dataset.store_id}</td>
-    <td>${dataset.store_location}</td>
-    <td>${dataset.product_id}</td>
-    <td>${dataset.unit_price}</td>
-    <td>${dataset.product_category}</td>
-    <td>${dataset.product_type}</td>
-    <td>${dataset.product_detail}</td>
-    </tr>`
+    // document.querySelector('#colom').innerHTML += `
+    // <tr>
+    // <td>${index + 1}</td>
+    // <td>${dataset.transaction_id}</td>
+    // <td>${dataset.transaction_date}</td>
+    // <td>${dataset.transaction_time}</td>
+    // <td>${dataset.transaction_qty}</td>
+    // <td>${dataset.store_id}</td>
+    // <td>${dataset.store_location}</td>
+    // <td>${dataset.product_id}</td>
+    // <td>${dataset.unit_price}</td>
+    // <td>${dataset.product_category}</td>
+    // <td>${dataset.product_type}</td>
+    // <td>${dataset.product_detail}</td>
+    // </tr>`
 }
 
+// table 
 let table = new DataTable('#all-table', {
-    // config options...
+    columns: [
+        {
+            data: 'DT_RowIndex',
+            name: 'DT_RowIndex',
+            defaultContent: ""
+        },
+        {
+            data: 'transaction_id',
+            name: 'transaction_id'
+        },
+        {
+            data: 'transaction_date',
+            name: 'transaction_date'
+        },
+        {
+            data: 'transaction_time',
+            name: 'transaction_time'
+        },
+        {
+            data: 'transaction_qty',
+            name: 'transaction_qty'
+        },
+        {
+            data: 'store_id',
+            name: 'store_id'
+        },
+        {
+            data: 'store_location',
+            name: 'store_location'
+        },
+        {
+            data: 'product_id',
+            name: 'product_id'
+        },
+        {
+            data: 'unit_price',
+            name: 'unit_price'
+        },
+        {
+            data: 'product_category',
+            name: 'product_category'
+        },
+        {
+            data: 'product_type',
+            name: 'product_type'
+        },
+        {
+            data: 'product_detail',
+            name: 'product_detail'
+        }
+
+    ],
+
+    data: datasets
 });
+
+table
+    .on('order.dt search.dt', function () {
+        let i = 1;
+ 
+        table
+            .cells(null, 0, { search: 'applied', order: 'applied' })
+            .every(function (cell) {
+                this.data(i++);
+            });
+    })
+    .draw();
+
 
 console.log(revenueByStore)
 document.querySelector('#bodyPerformance').innerHTML = htmlTopPerformance
@@ -152,11 +207,11 @@ let pieChart = createChart(
 
 
 // Helper function to get month name from date
-function getMonthName(dateString) {
-    const date = new Date(dateString);
-    const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    return monthNames[date.getMonth()];
-}
+// function getMonthName(dateString) {
+//     const date = new Date(dateString);
+//     const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+//     return monthNames[date.getMonth()];
+// }
 
 // Helper function to get month index from date
 function getMonthIndex(dateString) {
@@ -340,7 +395,7 @@ document.addEventListener('DOMContentLoaded', function () {
         datasetForChart.labels,
         datasetForChart.datasets,
         {
-            indexAxis: 'y', // This makes the bar chart horizontal
+            indexAxis: 'y', // bar chart horizontal
             responsive: true,
             scales: {
                 x: {
@@ -459,4 +514,122 @@ document.addEventListener('DOMContentLoaded', function () {
 // );
 
 
+document.querySelector('#store').addEventListener('change', event => {
+    console.log(event.target.value)
+    const value = event.target.value
+
+    let rows = datasets
+    if (value != 'All Option') {
+        rows = datasets.filter(row => row.store_location === value)
+    }
+    // console.log(rows)
+    table.clear()
+    table.rows.add(rows)
+    table.draw()
+    let topPerformance = []
+    let htmlTopPerformance = ""
+    let productSold = 0
+    let revenue = 0
+    let revenueByStore = []
+
+    for (let index = 0; index < rows.length; index++) {
+        const dataset = rows[index];
+        if (!topPerformance.includes(dataset.store_id)) {
+            topPerformance.push(dataset.store_id)
+            htmlTopPerformance += `
+        <tr>
+        <td>${dataset.store_location}</td>
+        <td>${dataset.store_id}</td>
+        </tr>
+        `
+        }
+        productSold += Number(dataset.transaction_qty)
+        revenue += Number(dataset.unit_price) * Number(dataset.transaction_qty)
+        if (revenueByStore.filter(row => row.id === dataset.store_id).length > 0) {
+            revenueByStore = revenueByStore.map(row => {
+                if (row.id === dataset.store_id) {
+                    return {
+                        ...row,
+                        revenue: row.revenue + (Number(dataset.unit_price) * Number(dataset.transaction_qty))
+                    }
+
+                }
+                return row;
+            })
+        }
+        else {
+            revenueByStore.push({
+                id: dataset.store_id,
+                name: dataset.store_location,
+                revenue: 0
+
+            })
+        }
+        // console.log(dataset)
+        //     document.querySelector('#colom').innerHTML += `
+        // <tr>
+        // <td>${index + 1}</td>
+        // <td>${dataset.transaction_id}</td>
+        // <td>${dataset.transaction_date}</td>
+        // <td>${dataset.transaction_time}</td>
+        // <td>${dataset.transaction_qty}</td>
+        // <td>${dataset.store_id}</td>
+        // <td>${dataset.store_location}</td>
+        // <td>${dataset.product_id}</td>
+        // <td>${dataset.unit_price}</td>
+        // <td>${dataset.product_category}</td>
+        // <td>${dataset.product_type}</td>
+        // <td>${dataset.product_detail}</td>
+        // </tr>`
+        // table.rows.add
+
+    }
+
+
+    document.querySelector('#bodyPerformance').innerHTML = htmlTopPerformance
+    document.querySelector('#sold').innerHTML = productSold
+    document.querySelector('#trans').innerHTML = rows.length
+    document.querySelector('#rev').innerHTML = revenue
+
+    let backgroundColor = []
+
+    if (value === 'Astoria') {
+        backgroundColor.push('rgb(255, 99, 132)')
+
+    }
+    else if (value === 'Lower Manhattan') {
+        backgroundColor.push('rgb(54, 162, 235)')
+
+    }
+    else if (value === "Hell's Kitchen") {
+        backgroundColor.push('rgb(255, 205, 86)')
+
+    }
+    else {
+        backgroundColor = [
+            'rgb(255, 99, 132)',
+            'rgb(54, 162, 235)',
+            'rgb(255, 205, 86)']
+    }
+    pieChart.data.labels = revenueByStore.map(item => item.name)
+    pieChart.data.datasets = [
+        {
+            label: 'trafic source',
+            data: persen(revenueByStore, revenue),
+            backgroundColor: backgroundColor
+        }]
+
+    pieChart.update()
+
+    console.log(pieChart.data)
+    console.log(revenueByStore.map(item => item.name))
+    console.log(persen(revenueByStore, revenue))
+    // data: persen(revenueByStore, revenue)
+
+
+
+    // bar chart 
+
+
+})
 
